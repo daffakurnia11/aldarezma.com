bd2aed16fbe6adc385276f0d2978e48f2d491205
Daffa Kurnia Fatah <daffakurniaf11@gmail.com>
2025-06-15 18:45:53 +0700

    feat(navbar): Create Navigation bar

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import React from "react";
+import Typography from "@/components/typography";
+import { MdOutlineClose, MdOutlineMenu } from "react-icons/md";
+import { motion } from "framer-motion";
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  return (
+    <div className="relative w-full h-[70px] bg-primary-600 flex items-center">
+      {/* Mobile Menu Navbar */}
+      <motion.div
+        initial={{ opacity: 0, y: "-100%" }}
+        animate={isOpen ? { opacity: 1, y: 180 } : { opacity: 0, y: "-100%" }}
+        transition={{ type: "tween", duration: 0.5, delay: 0.3 }}
+        className="absolute -z-10 flex flex-col items-center justify-center gap-8 bg-primary-600 w-full py-4 rounded-b-lg"
+      >
+        <button className="cursor-pointer">
+          <Typography.Paragraph size="lg" className="text-neutral-100">
+            Benefit
+          </Typography.Paragraph>
+        </button>
+        <button className="cursor-pointer">
+          <Typography.Paragraph size="lg" className="text-neutral-100">
+            Materi
+          </Typography.Paragraph>
+        </button>
+        <button className="cursor-pointer">
+          <Typography.Paragraph size="lg" className="text-neutral-100">
+            Testimoni
+          </Typography.Paragraph>
+        </button>
+        <button className="cursor-pointer">
+          <Typography.Paragraph size="lg" className="text-neutral-100">
+            Harga
+          </Typography.Paragraph>
+        </button>
+        <button className="cursor-pointer bg-secondary-600 rounded-base py-2 px-4">
+          <Typography.Paragraph
+            size="lg"
+            className="text-primary-600 font-medium"
+          >
+            Daftar Sekarang!
+          </Typography.Paragraph>
+        </button>
+      </motion.div>
+
+      <div className="container mx-auto flex items-center justify-between w-full bg-primary-600">
+        <Typography.Heading level={5} className="text-neutral-100">
+          AldaRezma
+        </Typography.Heading>
+        <button
+          className="md:hidden block cursor-pointer"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? (
+            <MdOutlineClose size={32} className="text-neutral-100" />
+          ) : (
+            <MdOutlineMenu size={32} className="text-neutral-100" />
+          )}
+        </button>
+
+        {/* Desktop Menu Navbar */}
+        <div className="md:flex hidden items-center justify-center w-fit gap-8">
+          <button className="cursor-pointer">
+            <Typography.Paragraph size="lg" className="text-neutral-100">
+              Benefit
+            </Typography.Paragraph>
+          </button>
+          <button className="cursor-pointer">
+            <Typography.Paragraph size="lg" className="text-neutral-100">
+              Materi
+            </Typography.Paragraph>
+          </button>
+          <button className="cursor-pointer">
+            <Typography.Paragraph size="lg" className="text-neutral-100">
+              Testimoni
+            </Typography.Paragraph>
+          </button>
+          <button className="cursor-pointer">
+            <Typography.Paragraph size="lg" className="text-neutral-100">
+              Harga
+            </Typography.Paragraph>
+          </button>
+          <button className="cursor-pointer bg-secondary-600 rounded-base py-2 px-4">
+            <Typography.Paragraph
+              size="lg"
+              className="text-primary-600 font-medium"
+            >
+              Daftar Sekarang!
+            </Typography.Paragraph>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
