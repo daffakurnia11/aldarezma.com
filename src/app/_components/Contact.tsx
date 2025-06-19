cec8c1eb8531a61e08cc73597e4ebf9f5986c431
Daffa Kurnia Fatah <daffakurniaf11@gmail.com>
2025-06-19 21:21:43 +0700

    feat(contact): Add link for contacts

--- src/app/_components/Contact.tsx
+++ src/app/_components/Contact.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { FaInstagram, FaRegEnvelope, FaWhatsapp } from "react-icons/fa6";
 import { LuPhone } from "react-icons/lu";
@@ -5,10 +6,18 @@ import { LuPhone } from "react-icons/lu";
 export default function Contact() {
   return (
     <div className="flex justify-center gap-6 md:gap-10 my-10">
-      <FaInstagram className="text-secondary-600 text-[32px] md:text-[56px]" />
-      <FaRegEnvelope className="text-secondary-600 text-[32px] md:text-[56px]" />
-      <FaWhatsapp className="text-secondary-600 text-[32px] md:text-[56px]" />
-      <LuPhone className="text-secondary-600 text-[32px] md:text-[56px]" />
+      <Link href={"https://www.instagram.com/aldarezma"} target="_blank">
+        <FaInstagram className="text-secondary-600 text-[32px] md:text-[56px]" />
+      </Link>
+      <Link href={"mailto:aldarzmdc@gmail.com"} target="_blank">
+        <FaRegEnvelope className="text-secondary-600 text-[32px] md:text-[56px]" />
+      </Link>
+      <Link href={"https://wa.me/62895411181899"} target="_blank">
+        <FaWhatsapp className="text-secondary-600 text-[32px] md:text-[56px]" />
+      </Link>
+      <Link href={"tel:+62895411181899"} target="_blank">
+        <LuPhone className="text-secondary-600 text-[32px] md:text-[56px]" />
+      </Link>
     </div>
   );
 }
