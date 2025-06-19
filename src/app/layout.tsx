868bf9c813f19508a860976c7330290fb521363a
Daffa Kurnia Fatah <daffakurniaf11@gmail.com>
2025-06-19 21:05:56 +0700

    feat(floating-whatsapp): Add floating whatsapp component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
+import { BsWhatsapp } from "react-icons/bs";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +15,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="relative">
+        {children}
+        <Link href={"https://wa.me/62895411181899"} target="_blank" className="z-50 fixed bottom-6 right-6 bg-white p-3 rounded-full shadow-lg shadow-primary-400 hover:bg-secondary-100 transition-all duration-300 cursor-pointer">
+          <BsWhatsapp size={40} className="text-[#128c7e] translate-x-[1px]" />
+        </Link>
+      </body>
     </html>
   );
 }
