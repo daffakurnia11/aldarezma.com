87d735feaaabdb1399f88617e2757bc1f1d9b3ee
Daffa Kurnia Fatah <daffakurniaf11@gmail.com>
2025-06-19 21:34:51 +0700

    fix(metadata): Change metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
 import LandingPage from "./_components/LandingPage";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "AldaRezma Workshop",
+  description: "TikTok Ads & CPAS bisa jadi mesin cuan kalau tahu cara mainnya",
+};
 
 export default function Home() {
   return <LandingPage />;
